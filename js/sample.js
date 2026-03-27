0190c99b000c9dff6408055d30cb6b77a6b81fe6
swamini <swaminibhandare10@gmail.com>
2026-03-27 23:34:51 +0530

    simple js

---
+++ js/sample.js
@@ -0,0 +1,13 @@
+function hi(){
+    console.log("Hi");
+    bye();
+}
+function bye(){
+    console.log("chala nikal");
+}
+function myname(){
+    console.log("Swamini");
+}
+function stuName(){
+    document.getElementById("input").innerHTML="<h1>swaminiiiiiii</h1>";
+}
