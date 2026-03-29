4f1327a6641e3b3e936a397daec59b1ae825deae
swamini <swaminibhandare10@gmail.com>
2026-03-29 21:49:14 +0530

    update

--- js/sample.js
+++ js/sample.js
@@ -1,13 +1,20 @@
 function hi(){
     console.log("Hi");
-    bye();
 }
 function bye(){
     console.log("chala nikal");
+    document.getElementById("in").innerHTML="<h1>bye chala nikal </h1>";
 }
+    
 function myname(){
     console.log("Swamini");
 }
 function stuName(){
     document.getElementById("input").innerHTML="<h1>swaminiiiiiii</h1>";
+}
+
+function enter(){
+     let stu = document.getElementById("input1").value;
+     console.log(stu);
+     document.getElementById("input").innerHTML="<h1>" + stu + "</h1>";
 }
